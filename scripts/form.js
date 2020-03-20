d2b9b3d6ba8a119dc1b9848c4caf5bb490634013
lorenzofelletti <lorenzo97.felletti@gmail.com>
2020-03-20 20:14:47 +0100

    bug fixex and improvements

--- scripts/form.js
+++ scripts/form.js
@@ -8,25 +8,24 @@ infectionprobability.addEventListener("change", inputChanged)
 
 
 function inputChanged() {
-    let arg = new Object();
-    let psize = parseInt(popsize.value);
-    let rectime = parseInt(recoverytimeinmillis.value);
-    let iprob = parseFloat(infectionprobability.value);
-    if ( psize != undefined && psize >= 3 && psize <= 300 )
-        arg.popsize = psize;
-    else 
-        arg.popsize = undefined;
-    if ( rectime )
-        arg.recoveryTimeInMillis = rectime;
-    else
-        arg.recoveryTimeInMillis = undefined;
-    if ( iprob )
-        arg.infectionProbability = iprob;
-    else
-        arg.infectionProbability = undefined;
-    console.log(arg);
-    reset(arg);
-    
+  let arg = new Object();
+  let psize = parseInt(popsize.value);
+  let rectime = parseInt(recoverytimeinmillis.value);
+  let iprob = parseFloat(infectionprobability.value);
+  if ( psize != undefined && psize >= 3 && psize <= 300 )
+    arg.popsize = psize;
+  else
+    arg.popsize = undefined;
+  if ( rectime )
+    arg.recoveryTimeInMillis = rectime;
+  else
+    arg.recoveryTimeInMillis = undefined;
+  if ( iprob )
+    arg.infectionProbability = iprob;
+  else
+    arg.infectionProbability = undefined;
+
+  sirsim.reset(arg); 
 }
 
 $('form').submit(false);
--- scripts/rangevalues.js
+++ scripts/rangevalues.js
@@ -1,6 +1,6 @@
 $(function() {
-    $("#ipmin").html( document.getElementById('infectionprobability').getAttribute('min') );
-    $("#ipmax").html( document.getElementById('infectionprobability').getAttribute('max') );
-    $("#rtmin").html( document.getElementById('recoverytimeinmillis').getAttribute('min') );
-    $("#rtmax").html( document.getElementById('recoverytimeinmillis').getAttribute('max') );    
+  $("#ipmin").html( document.getElementById('infectionprobability').getAttribute('min') );
+  $("#ipmax").html( document.getElementById('infectionprobability').getAttribute('max') );
+  $("#rtmin").html( document.getElementById('recoverytimeinmillis').getAttribute('min') );
+  $("#rtmax").html( document.getElementById('recoverytimeinmillis').getAttribute('max') );    
 });
