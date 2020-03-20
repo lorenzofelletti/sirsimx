838b0cd4fee7604b03043a902f9a4d17a9b12c51
lorenzofelletti <lorenzo97.felletti@gmail.com>
2020-03-20 14:53:22 +0100

    configure the two ranges

---
+++ scripts/rangevalues.js
@@ -0,0 +1,6 @@
+$(function() {
+    $("#ipmin").html( document.getElementById('infectionprobability').getAttribute('min') );
+    $("#ipmax").html( document.getElementById('infectionprobability').getAttribute('max') );
+    $("#rtmin").html( document.getElementById('recoverytimeinmillis').getAttribute('min') );
+    $("#rtmax").html( document.getElementById('recoverytimeinmillis').getAttribute('max') );    
+});
