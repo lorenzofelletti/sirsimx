7ee69b17ec32de8e210ef0912766babccffe4312
lorenzofelletti <lorenzo97.felletti@gmail.com>
2020-03-22 00:42:32 +0100

    added speed

--- scripts/rangevalues.js
+++ scripts/rangevalues.js
@@ -2,5 +2,7 @@ $(function() {
   $("#ipmin").html( document.getElementById('infectionprobability').getAttribute('min') );
   $("#ipmax").html( document.getElementById('infectionprobability').getAttribute('max') );
   $("#rtmin").html( document.getElementById('recoverytimeinmillis').getAttribute('min') );
-  $("#rtmax").html( document.getElementById('recoverytimeinmillis').getAttribute('max') );    
+  $("#rtmax").html( document.getElementById('recoverytimeinmillis').getAttribute('max') ); 
+  $("#spmin").html( document.getElementById('speed').getAttribute('min') );
+  $("#spmax").html( document.getElementById('speed').getAttribute('max') );   
 });
