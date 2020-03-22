0071b5551ef70167bb5c3edb395ddd79d992f6fb
lorenzofelletti <lorenzo97.felletti@gmail.com>
2020-03-22 22:16:30 +0100

    implemented speed range

--- scripts/form.js
+++ scripts/form.js
@@ -1,20 +1,23 @@
 var popsize = document.getElementById('popsize');
 var recoverytimeinmillis = document.getElementById('recoverytimeinmillis');
 var infectionprobability = document.getElementById('infectionprobability');
+var speedRange = document.getElementById('speed');
 var restartbtn = document.getElementById('restartbtn');
 popsize.addEventListener("change", inputChanged());
 popsize.addEventListener("mousewheel", inputChanged());
 recoverytimeinmillis.addEventListener("change", inputChanged());
 infectionprobability.addEventListener("change", inputChanged());
+speedRange.addEventListener("change", inputChanged());
 restartbtn.addEventListener("click", inputChanged());
 
 function inputChanged() {
-  let prev_psize, prev_rectime, prev_iprob;
+  let prev_psize, prev_rectime, prev_iprob, prev_speed;
   return function() {
     let arg = new Object();
     let psize = parseInt(popsize.value);
     let rectime = parseInt(recoverytimeinmillis.value);
     let iprob = parseFloat(infectionprobability.value);
+    let speed = parseFloat(speedRange.value);
     if ( psize != undefined && psize >= 3 && psize <= 300 ){
       arg.popsize = psize;
     }
@@ -32,7 +35,13 @@ function inputChanged() {
     } else {
       arg.infectionProbability = undefined;
     }
-    if ( prev_psize !== psize || prev_iprob !== iprob || prev_rectime !== rectime ) {
+    if ( speed ) {
+      arg.speed = speed;
+    } else {
+      arg.speed = undefined;
+    }
+    if ( prev_psize !== psize || prev_iprob !== iprob || 
+        prev_rectime !== rectime || prev_speed !== speed ) {
       sirsim.reset(arg);
     }
   }
