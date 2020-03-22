832634e7aaa9ea2844fd06759d55c74cb008d671
lorenzofelletti <lorenzo97.felletti@gmail.com>
2020-03-22 23:17:54 +0100

    ranges current values added

--- scripts/rangevalues.js
+++ scripts/rangevalues.js
@@ -4,5 +4,19 @@ $(function() {
   $("#rtmin").html( document.getElementById('recoverytimeinmillis').getAttribute('min') );
   $("#rtmax").html( document.getElementById('recoverytimeinmillis').getAttribute('max') ); 
   $("#spmin").html( document.getElementById('speed').getAttribute('min') );
-  $("#spmax").html( document.getElementById('speed').getAttribute('max') );   
+  $("#spmax").html( document.getElementById('speed').getAttribute('max') );
+
+  $('#rtactval').html ( document.getElementById('recoverytimeinmillis').value );
+  $('#ipactval').html ( document.getElementById('infectionprobability').value );
+  $('#spactval').html ( document.getElementById('speed').value );
+
+  recoverytimeinmillis.addEventListener( 'change', ( () => {
+    $('#rtactval').html ( document.getElementById('recoverytimeinmillis').value );
+  } ) );
+  infectionprobability.addEventListener( 'change', ( () => {
+    $('#ipactval').html ( document.getElementById('infectionprobability').value );
+  } ) );
+  speedRange.addEventListener( 'change', ( () => {
+    $('#spactval').html ( document.getElementById('speed').value );
+  } ) );
 });
