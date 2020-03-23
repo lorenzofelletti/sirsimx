5cb78c2048e495d923ff3c9ca2c663ebf025773b
lorenzofelletti <lorenzo97.felletti@gmail.com>
2020-03-23 19:57:18 +0100

    style changes

--- scripts/rangevalues.js
+++ scripts/rangevalues.js
@@ -1,4 +1,6 @@
 $( function() {
+  $("#psmin").html( document.getElementById('popsize').getAttribute('min') );
+  $("#psmax").html( document.getElementById('popsize').getAttribute('max') );
   $("#ipmin").html( document.getElementById('infectionprobability').getAttribute('min') );
   $("#ipmax").html( document.getElementById('infectionprobability').getAttribute('max') );
   $("#rtmin").html( document.getElementById('recoverytimeinmillis').getAttribute('min') );
@@ -6,10 +8,14 @@ $( function() {
   $("#spmin").html( document.getElementById('speed').getAttribute('min') );
   $("#spmax").html( document.getElementById('speed').getAttribute('max') );
 
+  $('#psactval').html ( document.getElementById('popsize').value );
   $('#rtactval').html ( document.getElementById('recoverytimeinmillis').value );
   $('#ipactval').html ( document.getElementById('infectionprobability').value );
   $('#spactval').html ( document.getElementById('speed').value );
 
+  popsize.addEventListener( 'change', ( () => {
+    $('#psactval').html ( document.getElementById('popsize').value );
+  } ) );
   recoverytimeinmillis.addEventListener( 'change', ( () => {
     $('#rtactval').html ( document.getElementById('recoverytimeinmillis').value );
   } ) );
