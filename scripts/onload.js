b37220f5068518186d21cd67981c23991665c83d
lorenzofelletti <lorenzo97.felletti@gmail.com>
2020-04-18 00:48:58 +0200

    graph and performance improvements

--- scripts/onload.js
+++
@@ -1,6 +0,0 @@
-function onLoadFunc() {
-  // !User agent === CHROME
-  if (navigator.userAgent.indexOf("Chrome") === -1) {
-    alert("This page works best on Chrome.\nIf you're havin problems using this page, try to open it in Chrome.");
-  }
-}
