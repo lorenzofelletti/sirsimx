e1e9b538b7ae587d04e352e1cde964714ef5e884
lorenzofelletti <lorenzo97.felletti@gmail.com>
2020-04-17 18:25:50 +0200

    Alrt if browser is not Chrome

---
+++ scripts/onload.js
@@ -0,0 +1,6 @@
+function onLoadFunc() {
+  // !User agent === CHROME
+  if (navigator.userAgent.indexOf("Chrome") === -1) {
+    alert("This page works best on Chrome.\nIf you're havin problems using this page, try to open it in Chrome.");
+  }
+}
